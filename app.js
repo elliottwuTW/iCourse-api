74d49a42238159e46ffb7ad40768af17ed54654c
elliottwuTW <elliott10009@gmail.com>
2021-01-14 11:16:44 +0800

    project init

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+  console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+})
